ef7ae40e7d27619c02b4e6647378a6ab3fff07af
Wasif <muhammadwasif2027@gmail.com>
2024-12-06 15:07:23 +0500

    Update listview.dart

--- lib/listview.dart
+++ lib/listview.dart
@@ -1,25 +1,23 @@
+
 import 'package:flutter/material.dart';
 
-class ListviewPage extends StatelessWidget {
+class ListViewTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('ListView Example')),
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: const Icon(Icons.person),
-            title: Text('Item ${index + 1}'),
-            subtitle: Text('Description of item ${index + 1}'),
-            onTap: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('Item ${index + 1} selected')),
-              );
-            },
-          );
-        },
-      ),
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.label),
+          title: Text('Item ${index + 1}'),
+          subtitle: Text('Subtitle ${index + 1}'),
+          onTap: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Tapped on Item ${index + 1}')),
+            );
+          },
+        );
+      },
     );
   }
 }
